d25d5e12d6f6956ab79dfff5b23573ab108439cc
Sam LR <sam.lindenrathen@gmail.com>
2012-05-22 11:34:39 +0900

    added air comparisons for muons at scint1 to script

--- scripts/degrader_comparison.C
+++ scripts/degrader_comparison.C
@@ -48,7 +48,15 @@ void degrader_comparison(){
     // save hists & muon trees
     // same for other charged particles ?
     // look for charged particles 
-    const TString file_roots [] = {"run_Aluminium_0.2mm",
+    const unsigned int n_files = 10;
+    const unsigned int n_funcs = 3;
+    
+    cut_func_ptr cuts[n_funcs] = {&stopped_muons,
+         &not_stopped_muons,
+         &all_scint1_muons};
+         
+    const TString air_root = "run_Air_5mm"; // background comparison for scint 1
+    const TString file_roots [n_files] = {"run_Aluminium_0.2mm",
         "run_Aluminium_10mm",
         "run_Aluminium_1mm",
         "run_Aluminium_2mm",
@@ -66,36 +74,50 @@ void degrader_comparison(){
     TString out_file_name = file_prefix + "stopped_saved_hists" + file_suffix;
     TFile* out_file = init_file(out_file_name, "RECREATE");
     // array of function pointers to use
-    cut_func_ptr cuts[] = {&stopped_muons, &not_stopped_muons};
-    const unsigned int n_files = 10;
-    TH1F* hists [n_files][2]; // one for stopped, one for not
 
+    TH1F* hists [n_files][n_funcs]; // one for stopped, one for not
+    
+    // initialise a hist for no degrader
+    TH1F* air_hist = new TH1F("Air", "Air", 50, 0, 200);
+    TH1F* tmp_array [] = {air_hist};
+    TFile* in_file = init_file(( file_prefix + air_root + file_suffix ));
+    in_branch_struct branch;
+    TTree* in_tree = init_tree<in_branch_struct>(in_file, "t", branch, &set_in_branch_address);
+    cut_func_ptr air_cut [] = {&all_scint1_muons};
+    loop_entries<in_branch_struct, TH1F>(in_tree, branch, tmp_array, 1, air_cut, true);
+    
     for(unsigned int file_no = 0; file_no < n_files; ++file_no) {
-        TH1F** h_pair = hists[file_no]; // alias to the pair of hists we want
+        TH1F** hist_set = hists[file_no]; // alias to the pair of hists we want
         
         cout << endl<< "Starting file "<< file_roots[file_no] << endl;
         // munge the filename
         TString resolved_filename = file_prefix + file_roots[file_no] + file_suffix;
         
         // open up and initialise things
-        TFile* in_file = init_file(resolved_filename);
-        in_branch_struct branch;
-        TTree* in_tree = init_tree<in_branch_struct>(in_file, "t", branch, &set_in_branch_address);
+        in_file = init_file(resolved_filename);
+        in_tree = init_tree<in_branch_struct>(in_file, "t", branch, &set_in_branch_address);
         
         // create the histograms then fill them in the loop
         out_file->cd(); // make sure they're added to the save file
         TString name1 = "stopped_"+file_roots[file_no]; // munge munge munge
         TString name2 = "not_stopped_"+file_roots[file_no];
-        h_pair[0] = new TH1F(name1, name1, 50, 0, 200); // fresh blood!
-        h_pair[1] = new TH1F(name2, name2, 50, 0, 200);
+        TString name3 = "compare_to_air_"+file_roots[file_no];
+        hist_set[0] = new TH1F(name1, name1, 50, 0, 200); // fresh blood!
+        hist_set[1] = new TH1F(name2, name2, 50, 0, 200);
+        hist_set[2] = new TH1F(name3, name3, 50, 0, 200);
         // loop-de-loop
-        loop_entries<in_branch_struct, TH1F>(in_tree, branch, h_pair, 2, cuts, true);
+        loop_entries<in_branch_struct, TH1F>(in_tree, branch, hist_set, n_funcs, cuts, true);
         
         // make pretty pictures! 
         TString h_title = "Muons stopped after " + file_roots[file_no];
         TString img_location = img_prefix + file_roots[file_no] + "_stopped" + img_suffix;
-        draw_pretty_two_hists(h_pair[0], h_pair[1], h_title, 
+        draw_pretty_two_hists(hist_set[0], hist_set[1], h_title, 
             "stopped", "not stopped", img_location);   
+            
+        h_title = "Comparison to Air of "+ file_roots[file_no];
+        img_location = img_prefix + file_roots[file_no] + "_post_degrader" + img_suffix;
+        draw_pretty_two_hists(air_hist, hist_set[2], h_title, 
+            "air", file_roots[file_no], img_location);
     }
     out_file->Write();
     
@@ -135,6 +157,43 @@ void set_in_branch_address(const in_branch_struct& branch, const TTree* tree) {
 template <class T> 
 bool equals(const T& a, const T& b){
     return a==b;
+}
+
+void all_scint1_muons(const in_branch_struct& branch, const TH1F* hist, const bool verbose){
+    // loop over all hits
+    // is it a muon or an electron
+    // if it's a muon is it in scint1?
+    //      has it already been seen?
+    //      if not add it's momentum & trackID to vector
+    // if it's an electron is it in scint1 or 2?
+    //      has it already been seen? 
+    //      if not does its parentID match a trackID in the vector?
+    //      if so Fill the hist with that momentum and add track to seen_e vec
+
+    const unsigned int n_hits = branch.g_nhit;
+    
+    if (n_hits == 0) return;
+    
+    vector<int> scint1_muons;
+    
+    for(unsigned int hit = 0; hit < n_hits; ++hit) {
+        int pid = branch.pdgid[hit];
+        int counter = branch.counter[hit];
+        int trackID = branch.trkid[hit];
+        
+        if ((abs(pid) == 13) && (counter == 1)) {
+            // muon in scint 1, have we already seen it? 
+            bool seen_it = is_in<int>(scint1_muons, trackID);
+
+            if (!seen_it) { 
+                // not seen it so add the track ID to the list 
+                // and record the momentum
+                scint1_muons.push_back(trackID);
+                double mom = length (branch.px[hit], branch.py[hit], branch.pz[hit]);
+                hist->Fill(mom);
+            } 
+        } 
+    }
 }
 
 void stopped_muons(const in_branch_struct& branch, const TH1F* hist, const bool verbose){
