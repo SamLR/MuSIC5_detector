baaa5956babfc035156ba297b33b82998b601440
Sam LR <sam.lindenrathen@gmail.com>
2012-06-04 18:19:17 +0900

    basic version of (hopefully) improved physics list)

---
+++ include/PhysicsList.hh
@@ -0,0 +1,32 @@
+//
+//  PhysicsList.hh
+//  music
+//
+//  Created by Sam Cook on 04/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef music_PhysicsList_hh
+#define music_PhysicsList_hh
+
+#include "G4VModularPhysicsList.hh"
+#include "globals.hh"
+
+class G4VPhysicsConstructor;
+class StepMax;
+class PhysicsListMessenger;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class PhysicsList: public G4VModularPhysicsList {
+public:
+    PhysicsList();
+    ~PhysicsList() {;};
+    // In theory should inherit everything we need
+    
+private:
+    void init();
+};
+
+
+#endif
