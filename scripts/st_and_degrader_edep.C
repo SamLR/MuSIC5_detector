3587263087f98853a48d7bf166e26c24b3138b79
Sam LR <sam.lindenrathen@gmail.com>
2012-06-06 14:35:49 +0900

    updated st_and_degrader for new file names

--- scripts/st_and_degrader_edep.C
+++ scripts/st_and_degrader_edep.C
@@ -4,8 +4,8 @@ void st_and_degrader_edep(){
     const unsigned int n_files = 2;
     // const unsigned int n_files = 17;
 
-    const TString file_roots [n_files] = { "st_1mm_1mm" ,
-        "st_10mm_10mm"};
+    const TString file_roots [n_files] = { "1mm_deg_1mm" ,
+        "10mm_deg_10mm"};
     // const TString file_roots [n_files] = { "st_1mm_1mm" ,
     //     "st_1mm_5mm" ,
     //     "st_1mm_10mm",
@@ -26,7 +26,7 @@ void st_and_degrader_edep(){
     //     "st_15mm_10mm",
     //     "st_15mm_15mm"};
 
-    const TString file_prefix = "../../output/st_and_degrader/st_and_degrader_";
+    const TString file_prefix = "../../output/st_and_degrader/st_and_degrader_st_";
     const TString img_prefix = "../../../images/st_and_degrader/";
     const TString save_file_name = "saved_hist";
 
