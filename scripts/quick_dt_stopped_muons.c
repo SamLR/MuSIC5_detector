df078050a18baa51f274232277434214b8783a4c
Sam LR <sam.lindenrathen@gmail.com>
2012-09-28 03:40:24 +0100

    basic dt plotter

--- scripts/quick_dt_stopped_muons.c
+++ scripts/quick_dt_stopped_muons.c
@@ -14,7 +14,7 @@ void set_addresses(const in_branch& mom, const TTree* tree);
 TH1F* make_hist(const TString* file_root);
 const int get_index(const int* array, const int length, const int target);
 
-void quick_dt_stoped_muons() {
+void quick_dt_stopped_muons() {
 	const int n_files = 6;
 	const TString file_roots [n_files] = { "Air_5mm",
 		"Aluminium_0.5mm",
@@ -42,8 +42,8 @@ void quick_dt_stoped_muons() {
 		for(unsigned int entry = 0; entry < n_entries; ++entry) {
 			in_tree->GetEntry(entry);
 
-			int muon_ids_scint1 [500];    // muons that have 'triggered'
-			int electron_ids [500];       // don't double count
+			int muon_ids_scint1 [500];	  // muons that have 'triggered'
+			int electron_ids [500];		  // don't double count
 			double muon_time_at_scint1[500]; // what we're actually going to plot
 			// index bounds; don't want to loop over 500 zeros
 			int n_seen_muons = 0;  
@@ -51,28 +51,27 @@ void quick_dt_stoped_muons() {
 
 			for(unsigned int hit = 0; hit < branch.n_hits; ++hit) {
 				// is it a muon in scint 1? 
-				//   first step?
-				//   add id to appropriate array
-				//   record time
-				//   ++ n_seen_scint1
+				//	 first step?
+				//	 add id to appropriate array
+				//	 record time
+				//	 ++ n_seen_scint1
 				// is it an electron in scint 1 or 2
-				//   first step
-				//   is its parent id in the scint1 array (and hence from target)
-				//   plot dt
-                const int pid = branch.pdgid[hit];
-                const int counter = branch.counter[hit];
-                const int id = branch.id[hit];
-                const bool first_step = branch.first_step[hit];
+				//	 first step
+				//	 is its parent id in the scint1 array (and hence from target)
+				//	 plot dt
+				const int pid = branch.pdgid[hit];
+				const int counter = branch.counter[hit];
+				const int id = branch.id[hit];
+				const bool first_step = branch.first_step[hit];
 				if (abs(pid) == 13) {
 					// muon stuff
-                    if (counter == 1 && first_step) {
+					if (counter == 1 && first_step) {
 						// first step in scint 1 now a triggering muon
 						const int index = get_index(muon_ids_scint1, n_seen_muons, id);
-
 						if (index == -1) {
-						    // new muon, log it!
+							// new muon, log it!
 							muon_ids_scint1[n_seen_muons] = id;
-							muon_time_at_scint1[n_seen_muons++] = branch.tof[hit]
+							muon_time_at_scint1[n_seen_muons++] = branch.tof[hit];
 						} 
 					}
 				} else if (abs(pid) == 11) {
@@ -86,7 +85,7 @@ void quick_dt_stoped_muons() {
 							const int parent_index = get_index(muon_ids_scint1, n_seen_muons, branch.parentid[hit]);
 							// have we seen its parent?
 							if (parent_index != -1) {
-                                const double dt = muon_time_at_scint1 - branch.tof[hit];
+								const double dt = branch.tof[hit] - muon_time_at_scint1[parent_index];
 								hists[file]->Fill(dt);
 							}
 						}
