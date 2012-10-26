1573e568d52ddba64a91836a7c2f1689d6832cf5
Sam LR <sam.lindenrathen@gmail.com>
2012-10-26 15:12:15 +0100

    Initial Commit

---
+++ Simple_muon_sim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Simple_muon_sim
+//
+//  Created by Sam Cook on 26/10/2012.
+//  Copyright (c) 2012 Sam Cook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
